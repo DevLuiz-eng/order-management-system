3c27932d612851dfa3cd29f3564a4ec41e74e138
DevLuiz-eng <luiznotbook2@gmail.com>
2026-01-06 17:10:28 -0300

    initialized Spring Boot project

---
+++ src/test/java/com/luizdev/order_management_system/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdev.order_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
